6ad21ba2ce69d21994345c270619a799f43b7490
mpv <193361506+mp-v99@users.noreply.github.com>
2025-11-02 22:24:13 -0600

    feat(core): add updateBoard function to process the player's move

--- script.js
+++ script.js
@@ -8,35 +8,54 @@ const gameSetup = (function settingBoard() {
                          ["[ ]", "[ ]", "[ ]"],
                          ["[ ]", "[ ]", "[ ]"]]
 
-    let playerOne = "0";
-    let playerTwo = "x"
+    const playerOne = "0"
+    const playerTwo = "x"
 
-    const getBoard = () => {console.log(matrixBoard)}
-    const getPlayerOne = () => {console.log(playerOne)}
-    const getPlayerTwo = () => {console.log(playerTwo)}
+    const getBoard = () => { return matrixBoard }
+
+    /* This function will receive the player's move to update the board and 
+     log a message with the update: */
+
+    const updateBoard = (player, marker, row, col) => {
+        matrixBoard[row].splice(col, 1, `[${marker}]`);
+        console.log(`${player}'s move:`)
+        console.log(matrixBoard)
+    }
+    const getPlayerOne = () => { return playerOne }
+    const getPlayerTwo = () => { return playerTwo }
 
     console.log(`The game is afoot. 
         Player One = 0
         Player Two = x
         
         The board: `)
-    getBoard()    
-     
-    return {getBoard, getPlayerOne, getPlayerTwo}
+
+
+    return { getBoard, updateBoard, getPlayerOne, getPlayerTwo }
 })()
 
 // This is the factory function for both of the players, it relies on closure
 // to prevent the record and the marker values from being accessed directly
 
-const createPlayer = function(name, playerMarker) {
+const createPlayer = function (name, playerMarker) {
     let record = 0;
-    let marker = playerMarker; 
+    let marker = playerMarker;
 
-    const getPlayerName = () => {return name}
-    const getPlayerMarker = () => {return marker}
-    const setPlayerMarker = () => {return marker} 
-    const getRecord = () => {return record}
-    const increaseRecord = () => {record++, getRecord()}
+    const getPlayerName = () => { return name }
+    const getPlayerMarker = () => { return marker }
+    const setPlayerMarker = () => { return marker }
+    const getRecord = () => { return record }
+    const increaseRecord = () => { record++, getRecord() }
 
-    return {getPlayerName, getPlayerMarker, setPlayerMarker, getRecord, increaseRecord}
+    return { getPlayerName, getPlayerMarker, setPlayerMarker, getRecord, increaseRecord }
 }
+
+
+const playerOne = createPlayer(prompt(`Player One: `), "0");
+const playerTwo = createPlayer(prompt(`Player Two: `), "x");
+
+console.log(`
+    ${playerOne.getPlayerName()} is: ${gameSetup.getPlayerOne()}
+    ${playerTwo.getPlayerName()} is: ${gameSetup.getPlayerTwo()}
+    `
+)
